9b3b9762794ef6d88ea37e44b9bd7810891e4b1e
NagiBear <andreysnytko13@gmail.com>
2022-01-10 01:37:13 +0300

    feat: added a dictionary in the form of a tree and added code that passes the test

--- src/index.js
+++ src/index.js
@@ -1,32 +1,80 @@
 module.exports = function toReadable (number) {
-    const obj = {
-        '0': 'zero',
-        '1': 'one',
-        '2': 'two',
-        '3': 'three',
-        '4': 'four',
-        '5': 'five',
-        '6': 'six',
-        '7': 'seven',
-        '8': 'eight',
-        '9': 'nine',
-        '10': 'ten',
-        '11': 'eleven',
-        '12': 'twelve',
-        '13': 'thirteen',
-        '14': 'fourteen',
-        '15': 'fifteen',
-        '16': 'sixteen',
-        '17': 'seventeen',
-        '18': 'eighteen',
-        '19': 'nineteen',
-        '20': 'twenty',
-        '30': 'thirty',
-        '40': 'forty',
-        '50': 'fifty',
-        '60': 'sixty',
-        '70': 'seventy',
-        '80': 'eighty',
-        '90': 'ninety'
-      };
+  const dictionary = {
+    1: 'one',
+    2: 'two',
+    3: 'three',
+    4: 'four',
+    5: 'five',
+    6: 'six',
+    7: 'seven',
+    8: 'eight',
+    9: 'nine',
+    next: {
+      1: {
+        1: 'eleven',
+        2: 'twelve',
+        3: 'thirteen',
+        4: 'fourteen',
+        5: 'fifteen',
+        6: 'sixteen',
+        7: 'seventeen',
+        8: 'eighteen',
+        9: 'nineteen',
+        0: 'ten'
+      },
+      2: 'twenty',
+      3: 'thirty',
+      4: 'forty',
+      5: 'fifty',
+      6: 'sixty',
+      7: 'seventy',
+      8: 'eighty',
+      9: 'ninety',
+      0: '',
+      next: {
+        1: 'one',
+        2: 'two',
+        3: 'three',
+        4: 'four',
+        5: 'five',
+        6: 'six',
+        7: 'seven',
+        8: 'eight',
+        9: 'nine',
+        0: 'zero'
+      }
+    }
+  }
+
+  const str = number.toString().split('')
+  let counter = str.length
+  let result = ''
+  while (counter) {
+    switch (counter) {
+      case 3: 
+        result += `${dictionary[str[str.length - counter]]} hundred `
+        break
+      case 2:
+        if (typeof dictionary.next[str[str.length - counter]] === 'string') {
+          if (str[str.length - counter + 1] !== 0)
+            result += `${dictionary.next[str[str.length - counter]]}`
+          if (str[str.length - counter] !== '0')
+            result += ' '
+        }
+        else {
+          result += `${dictionary.next['1'][str[str.length - counter + 1]]}`
+          counter--
+        }
+        break
+      case 1:
+        if ((result.length !== 0) && (str[str.length - counter] !== '0'))
+          result += `${dictionary.next.next[str[str.length - counter]]}`
+        else if (result.length === 0)
+          result += `${dictionary.next.next[str[str.length - counter]]}`
+        break
+    }
+    counter--
+  }
+  console.log(result)
+  return result.trim()
 }
